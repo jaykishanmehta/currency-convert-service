2b4d4d3f06d60c8b5161a299f08f82aceaa38d2e
jaykishanmehta <jaykishanmehta@IN-PG02NJS5.Nagarro.local>
2022-04-07 17:57:51 +0530

    Spring doc integration

--- src/main/java/com/nagarro/sb/CurrencyConvertApplication.java
+++ src/main/java/com/nagarro/sb/CurrencyConvertApplication.java
@@ -3,7 +3,11 @@ package com.nagarro.sb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Currency convert API", version = "1.0", description = "Converts currency based on available records in database"))
 public class CurrencyConvertApplication {
 
 	public static void main(String[] args) {
